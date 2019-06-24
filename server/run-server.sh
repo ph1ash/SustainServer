6537eb6624d8ba0ac46cb739a2b3005aac07362a
Matt Dresser <m.dres344@gmail.com>
2019-06-23 21:15:15 -0700

    Working server setup - get request retrieves POSTed sensor data. Allows users to post data as well

---
+++ server/run-server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /var/www/sustain/server/
+sudo npm start
--- server/server.js
+++ server/server.js
@@ -1,3 +1,12 @@
+const https = require("https"),
+	fs = require ("fs"),
+	helmet = require("helmet");
+
+const options = {
+	key: fs.readFileSync("/etc/letsencrypt/live/sustain.mattdresser.com/privkey.pem"),
+	cert: fs.readFileSync("/etc/letsencrypt/live/sustain.mattdresser.com/fullchain.pem")
+};
+
 var express = require('express'),
 	app = express(),
 	port = process.env.PORT || 3000;
@@ -9,12 +18,18 @@ var express = require('express'),
 		length: req.headers['content-length']
 })*/
 
+app.use(helmet());
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 var routes = require('./routes/sustainRoutes');
 routes(app);
 
-app.listen(port);
+https.createServer(options, app).listen(port);
 
-console.log('yarp');
+console.log('Starting Sustain Server');
